aafd4c534c8a6aac9d2e4645652fe32bb7e2ca32
hurt-chelar <gopikrishnamasy@gmail.com>
2022-12-28 23:03:19 +0530

    almost half of the website is done using tailwind

--- Tailwind-Project/tailwind.config.js
+++ Tailwind-Project/tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+      sm:'480px',
+      md:'760px',
+      lg:'970px',
+      xl:'1440px'
+
+    },
+    extend: {
+      colors:{
+        brightRed :'hsl(12,88%,59%)',
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    // ...
-  ],
+  plugins: [],
 }
